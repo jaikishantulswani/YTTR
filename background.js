0d36ae2cd8ee3b15ddde38f4a33bf85633fcc7e6
Jaikishan Tulswani <30144126+jaikishantulswani@users.noreply.github.com>
2023-10-10 11:21:33 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.runtime.onInstalled.addListener(function () {
+    // This code will be executed when the extension is installed or updated.
+});
+
---
+++ content.js
@@ -0,0 +1,15 @@
+var titleBar = document.querySelector('.ytp-title');
+if (titleBar) {
+    titleBar.remove();
+}
+
+var gradientTopElement = document.querySelector('.ytp-gradient-top');
+if (gradientTopElement) {
+    gradientTopElement.remove();
+}
+
+var chromeTopElement = document.querySelector('.ytp-chrome-top');
+if (chromeTopElement) {
+    chromeTopElement.remove();
+}
+
