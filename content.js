9cc3cef2a1f3ceff337a6053a448773cce9e8e71
Jaikishan Tulswani <30144126+jaikishantulswani@users.noreply.github.com>
2023-10-23 13:45:41 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,21 +1,26 @@
-function removeTitle() {
+function removeElements() {
     var titleBar = document.querySelector('.ytp-title');
     if (titleBar) {
         titleBar.remove();
+    }
+    
+    var gradientTop = document.querySelector('.ytp-gradient-top[data-layer="1"]');
+    if (gradientTop) {
+        gradientTop.remove();
     }
 }
 
 // Run the initial check when the DOM is ready
 if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', removeTitle);
+    document.addEventListener('DOMContentLoaded', removeElements);
 } else {
-    removeTitle();
+    removeElements();
 }
 
 // Observe changes in the DOM
 var observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
-        removeTitle();
+        removeElements();
     });
 });
 
